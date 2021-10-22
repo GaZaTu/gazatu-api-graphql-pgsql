a09dc39a03403c82e6394e16e5889ccb6a53a2e1
GaZaTu <dennis.ballmann@gmail.com>
2021-10-22 08:53:13 +0200

    add /stuff/isin-value/:isin

--- src/rest/index.ts
+++ src/rest/index.ts
@@ -2,6 +2,7 @@ import * as Router from '@koa/router'
 import { router as analyticsRouter } from './analytics/analytics-errors'
 import { router as triviaRouter } from './trivia/trivia-questions'
 import { router as blogRouter } from './blog/blog-entries'
+import { router as isinRouter } from './stuff/isin-value'
 
 export const router = new Router()
 
@@ -12,3 +13,5 @@ router
   .use(triviaRouter.allowedMethods())
   .use(blogRouter.routes())
   .use(blogRouter.allowedMethods())
+  .use(isinRouter.routes())
+  .use(isinRouter.allowedMethods())
