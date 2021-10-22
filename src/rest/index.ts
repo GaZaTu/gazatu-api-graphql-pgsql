a2d4ab35b7b99cab723b3d50971e6cbc6f135609
GaZaTu <dennis.ballmann@gmail.com>
2021-10-22 17:29:32 +0200

    Revert "add /stuff/isin-value/:isin"
    This reverts commit a09dc39a03403c82e6394e16e5889ccb6a53a2e1.

--- src/rest/index.ts
+++ src/rest/index.ts
@@ -2,7 +2,6 @@ import * as Router from '@koa/router'
 import { router as analyticsRouter } from './analytics/analytics-errors'
 import { router as triviaRouter } from './trivia/trivia-questions'
 import { router as blogRouter } from './blog/blog-entries'
-import { router as isinRouter } from './stuff/isin-value'
 
 export const router = new Router()
 
@@ -13,5 +12,3 @@ router
   .use(triviaRouter.allowedMethods())
   .use(blogRouter.routes())
   .use(blogRouter.allowedMethods())
-  .use(isinRouter.routes())
-  .use(isinRouter.allowedMethods())
